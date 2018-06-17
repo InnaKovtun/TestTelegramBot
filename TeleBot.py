0f7b5ddf7068b1bd9895b8d1298a19c7ad7f61f0
InnaKovtun <39884162+InnaKovtun@users.noreply.github.com>
2018-06-17 20:24:36 +0300

    Update TeleBot.py

--- TeleBot.py
+++ TeleBot.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from telebot import types
+from telepot.namedtuple import (ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, Filters)
+import polling
+import telepot
 import simplejson as json
 import requests
 import telebot
+import logging
+from json import dumps, loads, JSONEncoder, JSONDecoder
 
 app = Flask(__name__)
 bot = telebot.TeleBot('560420481:AAGSBQpupKG7fzhUkr905FuW6u-ORFvVK80')
@@ -13,61 +19,28 @@ URL = 'https://api.telegram.org/bot560420481:AAGSBQpupKG7fzhUkr905FuW6u-ORFvVK80
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    msg = bot.send_message(message.chat.id, 'Привет!\n Я - @cocopalmsalon_bot, личный помощник салона красоты "Коко Пальм"\n Рад тебя приветствовать! \n Для авторизации нажми, пожалуйста, кнопку "Отправить мой номер"')
-    bot.get_updates
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
+    button_phone = types.KeyboardButton(text="Отправить мой номер", request_contact=True)
+    keyboard.add(button_phone)
+    bot.send_message(message.chat.id,'Привет!\n Я - @cocopalmsalon_bot, личный помощник салона красоты "Коко Пальм"\n Рад тебя приветствовать! \n Для авторизации нажми, пожалуйста, кнопку "Отправить мой номер"', reply_markup=keyboard)
+
+
+def handle_messages(messages):
+	for message in messages:
+		if message.contact is not None:
+			bot.send_message(message.chat.id, 'Напиши свою дату рождения. Обещаю, я никому не скажу. Это будет нашим маленьким секретом (укажите свою дату рождения в формате ДД.ММ)')
+
+bot.set_update_listener(handle_messages)
+
 
 @bot.message_handler(content_types=["text"])
-def any_msg(message):
-keyboard = types.InlineKeyboardMarkup()
-	like_button= types.InlineKeyboardButton(text="Отправить мой номер", callback_data='like')
-	keyboard.add(like_button)
-	bot.send_photo(message.from_user.id,img, reply_markup=keyboard)
-
-#ФУНКЦИЯ КОТОРАЯ ПРИНЕМАЕТ ДАННЫЕ
-def write_json(date, filename='ansver.json'):
-	with open(filename, 'w') as f:
-		json.dump(date, f, indent=2, ensure_ascii=False)
-
-#ПОЛУЧЕНИЕ ДАННЫХ ОТ ТЕЛЕГРАМА
-def get_updates():
-	#https://api.telegram.org/bot531159192:AAGRUYM3DLLX6V7huZ9yxLBa7n5SwtR9DE0/getUpdates
-	url = URL + 'getUpdates'
-	r = requests.get(url)
-	#write_json(r.json())
-	return r.json()
-
-#ФУНКИЯ ДЛЯ ОТВЕТА бОТА
-def send_message(chat_id, text='bla-bla-bla'):
-	url = URL + 'sendMessage'
-	answer = {'chat_id': chat_id, 'text': text}
-	r = requests.post(url, json=answer)
-	return r.json()
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-	if request.method == 'POST':
-		r = request.get_json()
-		chat_id = r['message']['chat']['id']
-		message = r['message']['text']
-
-	if 'Здравствуйте' in message:
-		send_message(chat_id, text='Здравствуйте')
-
-		#whrite_json(r)
-		return jsonify(r)
-
-	return '<h1>Bot welcomes you</h1>'
-
-	#https://api.telegram.org/bot531159192:AAGRUYM3DLLX6V7huZ9yxLBa7n5SwtR9DE0/setWebhook?url=https://daa08a0a.ngrok.io/
-
-
-def main():
-	#r = requests.get(URL + 'getMe')
-	#write_json(r.json())
-	#r = get_updates()
-	#chat_id = r['result'][-1]['message']['chat']['id']
-	#send_message(chat_id)
-	pass
+def keyboard_menu(message):
+	if float(message.text):
+		markup = types.ReplyKeyboardMarkup()
+		markup.row('F.A.Q.', 'Заказать процедуру')
+		markup.row('Отзывы о работе мастеров', 'Портфолио')
+		markup.row('Хочу быть моделью')
+		bot.send_message(message.chat.id, "Спасибо за регистрацию, добро пожаловать в главное меню!", reply_markup=markup)
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+	bot.polling(none_stop=True)
